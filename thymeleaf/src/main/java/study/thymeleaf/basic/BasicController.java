c68fef9e4eec29f051d14a30457c008e5b84ddad
Jisu Lee <jisulee.shsf@gmail.com>
2024-07-23 22:53:54 +0900

    [타임리프1] 기본 제공 객체(업데이트 반영)

--- thymeleaf/src/main/java/study/thymeleaf/basic/BasicController.java
+++ thymeleaf/src/main/java/study/thymeleaf/basic/BasicController.java
@@ -1,6 +1,10 @@
 package study.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,8 +50,27 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model,
+                               HttpServletRequest request,
+                               HttpServletResponse response,
+                               HttpSession session) {
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        session.setAttribute("sessionData", "hello session!");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "hello " + data;
+        }
+    }
+
     @Data
-    public static class User {
+    static class User {
         private String username;
         private int age;
 
